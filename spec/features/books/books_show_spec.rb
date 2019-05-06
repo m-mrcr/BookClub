68d86cd27e1f13eb73972cc266145ca85f0d7325
James Cape <james.cape@gmail.com>
2019-05-06 16:34:47 -0600

    Adds click_link to test
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_show_spec.rb
+++ spec/features/books/books_show_spec.rb
@@ -6,8 +6,10 @@ describe "user sees one book" do
 
       book_1 = Book.create!(title: "Title 1", author: "Author 1", pages: 300, year: 1963, cover_url:"https://images-na.ssl-images-amazon.com/images/I/81HNeEElO8L.jpg")
       book_2 = Book.create!(title: "Title 2", author: "Author 2", pages: 400, year: 1970, cover_url:"https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Into_the_Wild_%28book%29_cover.png/220px-Into_the_Wild_%28book%29_cover.png")
-
+# require 'pry'; binding.pry
       visit books_path
+
+      click_link book_1.title
 
       expect(page).to have_content(book_1.title)
       expect(page).to have_content(book_1.author)
