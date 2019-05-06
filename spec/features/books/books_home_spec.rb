8018570329f9d1a1a978699ba7c5c2604497ab2b
James Cape <james.cape@gmail.com>
2019-05-06 16:34:47 -0600

    Restricts resources to only index and show
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_home_spec.rb
+++ spec/features/books/books_home_spec.rb
@@ -6,7 +6,7 @@ describe "user sees all books" do
       book_1 = Book.create!(title: "Title 1", pages: 300, year: 1963, cover_url:"https://images-na.ssl-images-amazon.com/images/I/81HNeEElO8L.jpg")
       book_2 = Book.create!(title: "Title 2", pages: 400, year: 1970, cover_url:"https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Into_the_Wild_%28book%29_cover.png/220px-Into_the_Wild_%28book%29_cover.png")
 
-      visit '/books'
+      visit books_path
 
       expect(page).to have_link(book_1.title)
       expect(page).to have_link(book_2.title)
