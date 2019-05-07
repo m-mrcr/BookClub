cc5e76b834969b76c99589a5af3f92772da1343b
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-07 14:46:19 -0600

    adds table for users in db
    Co-authored-by: James Cape <james.cape@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507174058) do
+ActiveRecord::Schema.define(version: 20190507203142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,19 @@ ActiveRecord::Schema.define(version: 20190507174058) do
     t.string "cover_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "headline"
+    t.text "body"
+    t.integer "rating"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
   end
 
   add_foreign_key "author_books", "authors"
