6cacca5dc454f6b4bfbdaae439db0b8ec2d3d5ef
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-07 15:55:37 -0600

    Adds user relation and removes validation for presence of username
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :book
-  validates_presence_of :headline, :body, :rating, :username
+  belongs_to :user
+  validates_presence_of :headline, :body, :rating
 end
