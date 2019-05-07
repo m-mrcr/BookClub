a5b427bac7393e4be5d27929b6592492a3001b22
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-07 15:52:00 -0600

    Adds reviews relation and removes presence of :author
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
-  validates_presence_of :title, :author, :pages, :year, :cover_url
-
+  has_many :reviews
   has_many :author_books
   has_many :authors, through: :author_books
+  validates_presence_of :title, :pages, :year, :cover_url
 end
