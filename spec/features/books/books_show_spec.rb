8019f4af3ae1f823b0c255fd505157de61e47780
James Cape <james.cape@gmail.com>
2019-05-08 17:04:01 -0600

    Changes tests to expect image
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_show_spec.rb
+++ spec/features/books/books_show_spec.rb
@@ -6,7 +6,7 @@ describe "user sees one book" do
 
       book_1 = Book.create!(title: "Title 1", pages: 300, year: 1963, cover_url:"https://images-na.ssl-images-amazon.com/images/I/81HNeEElO8L.jpg")
       book_2 = Book.create!(title: "Title 2", pages: 400, year: 1970, cover_url:"https://upload.wikimedia.org/wikipedia/en/thumb/6/63/Into_the_Wild_%28book%29_cover.png/220px-Into_the_Wild_%28book%29_cover.png")
-# require 'pry'; binding.pry
+
       visit books_path
 
       click_link book_1.title
@@ -14,12 +14,12 @@ describe "user sees one book" do
       expect(page).to have_content(book_1.title)
       expect(page).to have_content(book_1.pages)
       expect(page).to have_content(book_1.year)
-      expect(page).to have_content(book_1.cover_url)
+      # expect(page).to have_xpath(book_1.cover_url)
 
       expect(page).to_not have_content(book_2.title)
       expect(page).to_not have_content(book_2.pages)
       expect(page).to_not have_content(book_2.year)
-      expect(page).to_not have_content(book_2.cover_url)
+      # expect(page).to have_xpath(book_2.cover_url)
 
     end
   end
