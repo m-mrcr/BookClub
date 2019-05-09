34165b0aa03febb8415c03705ee804a05d7dd8ab
James Cape <james.cape@gmail.com>
2019-05-09 14:42:07 -0600

    Adds test for multiple authors
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_new_spec.rb
+++ spec/features/books/books_new_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "user inputs new book" do
   describe "they link to new from the books index" do
-    it "displays a form to enter a new book" do
+    it "displays a form to enter a new book for one author" do
       visit books_path
       click_link "New Book"
 
@@ -23,6 +23,30 @@ describe "user inputs new book" do
 
       expect(page).to have_content(island_title)
       expect(current_path).to eq(books_path)
+    end
+
+    it "displays a form to enter a new book for multiple authors" do
+      visit books_path
+      click_link "New Book"
+
+      expect(current_path).to eq(new_book_path)
+
+      island_title = "Island"
+      island_year = 1990
+      island_pages = 234
+      island_cover_url = "https://en.wikipedia.org/wiki/File:Island.JPG"
+      island_authors = "Aldus Huxley, Albus Dumbledore"
+
+      fill_in "book[title]", with: island_title
+      fill_in "book[year]", with: island_year
+      fill_in "book[pages]", with: island_pages
+      fill_in "book[cover_url]", with: island_cover_url
+      fill_in  "book[authors]", with: island_authors
+      click_on "Submit Book"
+
+      expect(page).to have_content(island_title)
+      expect(current_path).to eq(books_path)
+
     end
   end
 end
