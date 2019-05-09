5541e3ef92b698bf23cd2be48a7564eee0c1b35b
James Cape <james.cape@gmail.com>
2019-05-09 11:25:38 -0600

    Changes test to create variables instead of object
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_new_spec.rb
+++ spec/features/books/books_new_spec.rb
@@ -3,20 +3,29 @@ require "rails_helper"
 describe "user inputs new book" do
   describe "they link to new from the books index" do
     it "displays a form to enter a new book" do
-      island = Book.create(title: "Island", pages: 313, year: 2018, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      visit books_path
+      click_link "New Book"
 
+      expect(current_path).to eq(new_book_path)
 
-      visit "/books/new"
+      island_title = "Island"
+      island_year = 1990
+      island_pages = 234
+      island_cover_url = "https://en.wikipedia.org/wiki/File:Island.JPG"
+      island_authors = "Aldus Huxley"
 
-      # fill_in "book[title]", with:
-      # fill_in "book[year]", with: 1980
-      fill_in "book[pages]", with: 450
-      fill_in "book[cover_url]", with: new_book
-      fill_in "book[authors]", with: new_book
+      # island = Book.create(title: "Island", pages: 313, year: 2018, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      # island.authors << Author.create(name: 'Aldus Huxley')
 
+      fill_in "book[title]", with: island_title
+      fill_in "book[year]", with: island_year
+      fill_in "book[pages]", with: island_pages
+      fill_in "book[cover_url]", with: island_cover_url
+      fill_in  "book[authors]", with: island_authors
       click_on "Submit Book"
 
-      expect(page).to have_content(new_book)
+      expect(page).to have_content(island_title)
+      expect(current_path).to eq(books_path)
     end
   end
 end
