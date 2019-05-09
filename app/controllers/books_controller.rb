4f648aa155efcf4a4ec8df6350c8ef5161666eb3
James Cape <james.cape@gmail.com>
2019-05-09 11:26:43 -0600

    Adds create and private methods
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    # require 'pry'; binding.pry
   end
 
   def show
@@ -9,7 +10,21 @@ class BooksController < ApplicationController
   end
 
   def new
+    # require 'pry'; binding.pry
     @book = Book.new
+  end
+
+  def create
+    # require 'pry'; binding.pry
+    @book = Book.create!(book_params)
+    @book.save
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :year, :pages, :cover_url, :authors)
   end
 
 end
