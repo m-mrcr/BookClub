9ef0d7bd306e19adc601d13eb89830aebac3baef
James Cape <james.cape@gmail.com>
2019-05-09 13:04:47 -0600

    Removes pry lines. Creates book and author from param attributes
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,6 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    # require 'pry'; binding.pry
   end
 
   def show
@@ -10,21 +9,27 @@ class BooksController < ApplicationController
   end
 
   def new
-    # require 'pry'; binding.pry
     @book = Book.new
   end
 
   def create
+    # create_author = Author.create(name: book_params[:authors])
+    # create_book = Book.create(title: book_params[:title], year: book_params[:year], pages: book_params[:pages], cover_url: book_params[:cover_url], authors: [create_author])
     # require 'pry'; binding.pry
-    @book = Book.create!(book_params)
-    @book.save
+
+    author = Author.find_or_create_by(name: params[:book][:authors])
+    book = Book.new(book_params)
+    book.authors << author
+    book.save
+    # require 'pry'; binding.pry
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :year, :pages, :cover_url, :authors)
+    # params.require(:book).permit(:title, :year, :pages, :cover_url, :authors)
+    params.require(:book).permit(:title, :year, :pages, :cover_url)
   end
 
 end
