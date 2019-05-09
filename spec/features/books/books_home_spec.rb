9795fd2da141afcf51187686e59390f3501c4182
James Cape <james.cape@gmail.com>
2019-05-09 09:44:50 -0600

    Adds tests for new book button
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_home_spec.rb
+++ spec/features/books/books_home_spec.rb
@@ -29,5 +29,22 @@ describe "user sees all books" do
       expect(page).to have_xpath("//img[contains(@src,'#{astronaut.cover_url}')]")
       expect(page).to have_xpath("//img[contains(@src,'#{css.cover_url}')]")
     end
+
+    it "displays a button that directs to a new book creation page" do
+      astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      css = Book.create(title: 'CSSucks', pages: 420, year: 2019, cover_url: 'https://i1.wp.com/www.developermemes.com/wp-content/uploads/2014/01/CSS-Sucks-TShirt-Meme.jpg?resize=385%2C232')
+
+      astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
+      css.authors << Author.find_or_create_by(name: 'Matt Weiss')
+
+      visit books_path
+
+      expect(page).to have_link("New Book")
+
+      click_link("New Book")
+
+      expect(current_path).to eq('books/new')
+
+    end
   end
 end
