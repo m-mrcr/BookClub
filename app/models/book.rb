bdc2dc970ebda4edee41f12f38ea1450db661d43
James Cape <james.cape@gmail.com>
2019-05-09 16:59:11 -0600

    Adds titles method
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
   validates_presence_of :title, :pages, :year, :cover_url
+
+  def self.titles
+    self.select(:title).pluck(:title)
+  end
 end
