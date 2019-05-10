a8e8b4d0fedd0dfb9694ff17d405a1bd8a233108
James Cape <james.cape@gmail.com>
2019-05-09 20:19:15 -0600

    Adds placeholder methods for user
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
   has_many :reviews
   validates_presence_of :username
+
+  def user_review_count
+
+  end
+
+  def users_by_review_count
+
+  end
+
+  def self.most_prolific_users
+    
+  end
+
 end
