19df0a0bd04418e7912d0c95b1a08b08407faea6
James Cape <james.cape@gmail.com>
2019-05-11 15:51:58 -0600

    adds implementation for creating a new review
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,28 +2,33 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @book = Book.find(params[:book_id])
   end
 
   def create
+    @book = Book.find(params[:book_id])
     review = Review.new(review_params)
-    username = params[:review][:username].titleize
-    if Review.usernames.include?(username) || params[:review][:rating].to_i != (1..5)
-      redirect_to new_review_path
+    username = user_params[:username].titleize
+    if User.usernames.include?(username) || !params[:review][:rating].to_i.between?(1,5)
+      redirect_to new_book_review_path
     else
       formatted_username = username.downcase.strip.titleize
       review_user = User.find_or_create_by!(username: formatted_username)
-      review.user << review_user
-
-      binding.pry
+      review_user.reviews << review
+      @book.reviews << review
       review.save
-      redirect_to book_path()
+      redirect_to book_path(params[:book_id])
     end
   end
 
 private
 
   def review_params
-    params.require(:review).permit(:headline, :username, :rating, :body)
+    params.require(:review).permit(:headline, :rating, :body)
+  end
+
+  def user_params
+    params.permit(:username)
   end
 
 end
