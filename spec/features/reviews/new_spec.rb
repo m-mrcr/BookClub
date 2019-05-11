ba3e8fc5a3c2a8a9c64a322c03c6b59f56c55b91
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-11 13:05:27 -0600

    Creates tests for new review
    Co-authored-by: James Cape <james.cape@gmail.com>

---
+++ spec/features/reviews/new_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+describe "user inputs new review" do
+  describe "links to new from book show page" do
+    it "displays a link on book show page for Write Review" do
+      astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
+      css = Book.create(title: 'CSSucks', pages: 420, year: 2019, cover_url: 'https://i1.wp.com/www.developermemes.com/wp-content/uploads/2014/01/CSS-Sucks-TShirt-Meme.jpg?resize=385%2C232')
+      css.authors << Author.find_or_create_by(name: 'Matt Weiss')
+
+      visit books_path
+
+      click_link(astronaut.title)
+
+      expect(page).to have_link("Write Review")
+    end
+
+    it "displays a form to enter new review when clicking on link to Write Review" do
+      astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
+      css = Book.create(title: 'CSSucks', pages: 420, year: 2019, cover_url: 'https://i1.wp.com/www.developermemes.com/wp-content/uploads/2014/01/CSS-Sucks-TShirt-Meme.jpg?resize=385%2C232')
+      css.authors << Author.find_or_create_by(name: 'Matt Weiss')
+
+      visit books_path
+
+      click_link(astronaut.title)
+      click_link("Write Review")
+
+      expect(current_path).to eq(new_review_path)
+
+      review_headline = "Some Bubkus Review"
+      review_username = "LittleKitty9000"
+      review_rating = 4
+      review_body = "I know nothing about this book, I've never read it."
+
+      fill_in "review[headline]", with: review_headline
+      fill_in "review[username]", with: review_username
+      fill_in "review[rating]", with: review_rating
+      fill_in "review[body]", with: review_body
+      click_on "Submit Review"
+
+      expect(page).to have_content(review_headline)
+      expect(current_path).to eq("/books/#{astronaut.id}")
+      expect(page).to_not have_content("CSSucks")
+      expect(current_path).to_not eq("/books/#{css.id}")
+    end
+
+  end
+end
