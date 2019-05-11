3e4d4ba9180eb7a6868fd5f332a15bf0826c2d76
James Cape <james.cape@gmail.com>
2019-05-10 18:28:35 -0600

    Tests current path reflects having clicked on the book title
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/books/books_show_spec.rb
+++ spec/features/books/books_show_spec.rb
@@ -13,10 +13,11 @@ describe "user sees one book" do
       user_3 = User.create(username: "Fiddlestick Calldispatch")
       user_3.reviews.create(book: hunger, body: "This is an interesting treatise on food theory and utopian food consumption", headline: "Hungry hungry killers", rating: 3)
 
-
       visit books_path
 
       click_link astronaut.title
+
+      expect(current_path).to eq(book_path(astronaut.id))
 
       expect(page).to have_content(astronaut.title)
       expect(page).to have_content(astronaut.authors[0].name)
@@ -33,7 +34,44 @@ describe "user sees one book" do
       expect(page).to_not have_xpath("//img[contains(@src,'#{hunger.cover_url}')]")
       expect(page).to_not have_content("Fiddlestick Calldispatch")
       expect(page).to_not have_content("This is an interesting treatise")
+    end
 
+    it "displays the top three reviews" do
+      astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+
+      astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
+
+      user_1 = User.create(username: "Snorkeldink Crumplehorn")
+      user_2 = User.create(username: "Fiddlestick Calldispatch")
+      user_3 = User.create(username: "Burlington Anglerfish")
+      user_4 = User.create(username: "Birdbox Bandersnatch")
+
+      user_1.reviews.create(book: astronaut, body: "I have so many good things to say about this book I don't think they'll all fit into one review (for my full review, including my four-year-old's reaction to it, please visit my blog, Cozy Little Book Journal).", headline: 'I have so many good things to say about this book', rating: 1)
+      user_2.reviews.create(book: astronaut, body: "This is an interesting treatise on food theory and utopian food consumption", headline: "Hungry hungry killers", rating: 2)
+      user_3.reviews.create(book: astronaut, body: "Agriculture Secretary Thomas J. Vilsack", headline: "Photoshop Tips From", rating: 4)
+      user_4.reviews.create(book: astronaut, body: "Make Psychologists Feel Ashamed", headline: "Shocking Things That", rating: 3)
+
+      visit books_path
+
+      click_link astronaut.title
+# require 'pry'; binding.pry
+      expect(current_path).to eq(book_path(astronaut.id))
+
+      # expect(page).to have_content(astronaut.title)
+      # expect(page).to have_content(astronaut.authors[0].name)
+      # expect(page).to have_content(astronaut.pages)
+      # expect(page).to have_content(astronaut.year)
+      #
+      # expect(page).to have_xpath("//img[contains(@src,'#{astronaut.cover_url}')]")
+      # expect(page).to have_content("Snorkeldink Crumplehorn")
+      # expect(page).to have_content("I have so many good things")
+      #
+      # expect(page).to_not have_content(hunger.title)
+      # expect(page).to_not have_content(hunger.pages)
+      # expect(page).to_not have_content(hunger.year)
+      # expect(page).to_not have_xpath("//img[contains(@src,'#{hunger.cover_url}')]")
+      # expect(page).to_not have_content("Fiddlestick Calldispatch")
+      # expect(page).to_not have_content("This is an interesting treatise")
     end
   end
 end
