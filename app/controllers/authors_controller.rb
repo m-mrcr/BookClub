09fabb99292cc96707a124546d39de97a6f2a417
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-11 11:05:31 -0600

    adds ivar for alternate_authors in author controller
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,17 @@
 class AuthorsController < ApplicationController
 
   def show
+    # @all_authors = Author.all.where(book.id=)
     @author = Author.find(params[:id])
     @books = @author.books
+    @alternate_authors = []
+    @books.each do |book|
+      book.authors.each do |author|
+        unless @author.name == author.name
+          @alternate_authors << author
+        end
+      end
+    end
   end
 
 end
