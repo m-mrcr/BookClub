38e1422801a4bdeac7995636c54adf840adba006
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-11 13:06:18 -0600

    Deletes unnecessary commented-out code
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,6 @@ class User < ApplicationRecord
     reviews.count
   end
 
-  # def by_review_count
-  #   user.joins(:reviews).select("COUNT(reviews.id)")
-  #   count(:reviews)
-  # end
-
-
-# SELECT  users.username,
-# COUNT(reviews.id) AS review_count
-# FROM "users" INNER JOIN "reviews" ON "reviews"."user_id" = "users"."id"
-# GROUP BY "users"."id"
-# ORDER BY review_count DESC LIMIT(3);
-
   def self.most_prolific
     select("users.*, COUNT(reviews.id) AS review_count")
       .joins(:reviews)
