62a6b8abdfd70d0a04130bff80de523ec960a72d
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-11 11:04:54 -0600

    adds test for alternate authors on author show page
    Co-authored-by: James Cape <james.cape@gmail.com>

--- spec/features/author/show_spec.rb
+++ spec/features/author/show_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 describe "user sees one author" do
   describe "they link from the books index" do
-
-
     it "displays information for one author" do
       astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
       astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
@@ -23,6 +21,21 @@ describe "user sees one author" do
       expect(page).to_not have_content(css.pages)
       expect(page).to_not have_content(css.year)
       expect(page).to_not have_xpath("//img[contains(@src,'#{css.cover_url}')]")
+    end
+
+    it "displays information for one author with co-written book" do
+      astronaut = Book.create(title: "An Astronaut's Guide to Life on Earth", pages: 284, year: 2013, cover_url: 'http://media.npr.org/assets/bakertaylor/covers/a/an-astronauts-guide-to-life-on-earth/9780316253017_custom-72b5b1e3d259fb604fee1401424db3c8cd04cfe0-s6-c30.jpg')
+      astronaut.authors << Author.find_or_create_by(name: 'Chris Hadfield')
+      astronaut.authors << Author.find_or_create_by(name: 'Matt Weiss')
+
+      visit books_path
+
+      click_link(astronaut.authors[0].name)
+
+      within '#alternate-authors' do
+        expect(page).to have_link(astronaut.authors[1].name)
+        expect(page).to_not have_link(astronaut.authors[0].name)
+      end
     end
   end
 end
