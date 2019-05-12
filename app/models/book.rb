95f4672487b502e2ca59f85e5868f978ea140c7f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-12 17:17:27 -0600

    updates routes and model relationships to include destroy/delete
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :author_books
   has_many :authors, through: :author_books
   validates_presence_of :title, :pages, :year, :cover_url
