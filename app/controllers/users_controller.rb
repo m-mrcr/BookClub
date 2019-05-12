e03952a48e3b7d94d2590a48cc7155f4c555b4a9
James Cape <james.cape@gmail.com>
2019-05-12 14:32:49 -0600

    Adds nearly correct param sort logic
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    case params[:sort]
+    when "ascend"
+      # require 'pry'; binding.pry
+      @reviews = Review.where(user_id: params[:id]).sort_oldest_first
+    when "descend"
+      # require 'pry'; binding.pry
+      @reviews = Review.where(user_id: params[:id]).sort_newest_first
+    else
+      @reviews = Review.where(user_id: params[:id])
+    end
   end
 
 end
