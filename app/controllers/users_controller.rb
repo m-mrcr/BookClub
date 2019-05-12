2f76f9b21d44f3a6dc29a98fcd8bbdc8760bf023
James Cape <james.cape@gmail.com>
2019-05-12 15:29:27 -0600

    Replaces activerecord methods with model methods
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @reviews = Review.where(user_id: params[:id])
     case params[:sort]
     when "ascend"
-      # require 'pry'; binding.pry
-      @reviews = @reviews.order(:created_at)
+      @reviews = @user.sort_oldest_first
     when "descend"
-      # require 'pry'; binding.pry
-      @reviews = @reviews.order(:created_at).reverse_order
+      @reviews = @user.sort_newest_first
     else
+      @reviews = @user.sort_newest_first
     end
   end
-
 end
