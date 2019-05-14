4d0020cffdc4053ae8facbaca9387e3a39539407
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-14 08:54:55 -0600

    adds alternate_authors method to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,9 @@ class Author < ApplicationRecord
   has_many :author_books, dependent: :destroy
   has_many :books, through: :author_books
   validates_presence_of :name
+
+  def self.alternate_authors(author)
+    where.not(id: author.id)
+  end
+
 end
