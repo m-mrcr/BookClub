2080c80a0b198aa87f0cd8647ab2ed7238b9f24c
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-14 11:03:03 -0600

    completes review model testing
    Co-authored-by: James Cape <james.cape@gmail.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,12 +2,4 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
   validates_presence_of :headline, :body, :rating
-
-  ################ Begin "NEEDS TESTS"########################
-
-  def username
-    user.name
-  end
-
-
 end
