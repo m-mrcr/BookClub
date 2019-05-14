972c4f298e268b5ae01a8cf3be804c03d52a1851
James Cape <james.cape@gmail.com>
2019-05-14 09:37:43 -0600

    Changes test to click on Books Central
    Co-authored-by: Martin Mercer <28820023+m-mrcr@users.noreply.github.com>

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,7 +5,14 @@ describe "user sees a homepage" do
     visit '/'
 
     expect(page).to have_content("Welcome!")
-    expect(page).to have_link("Continue to Books")
+    expect(page).to have_link("Books Central")
     expect(page.status_code).to be(200)
+  end
+
+  it "leads user to /books when clicking Books Central" do
+    visit '/'
+    click_link("Books Central")
+
+    expect(current_path).to eq(books_path)
   end
 end
